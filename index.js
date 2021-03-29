62303abe206f5ec7227215e229b5ce8e9638d87c
Naveed Choudhury <naveedchoudhury24@gmail.com>
2021-03-29 16:22:06 -0400

    commit tree 9f75088fd9df421ab7ab334d5ff2c1c67705a5d3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(star = "*") {
+    return function (word = "a hard worker") {
+        return `You are ${star}${word}${star}!` 
+    }
+}
